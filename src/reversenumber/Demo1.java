6e417da62ac0573a0470cbaff288c359382faac5
Rohit <khandarerohitrk@gmail.com>
2021-04-02 22:35:19 +0530

    added,deleted.modified

--- src/reversenumber/Demo1.java
+++ src/reversenumber/Demo1.java
@@ -7,7 +7,7 @@ public class Demo1 {
 	public static void main(String[] args) 
 	{
 		Scanner sc1 = new Scanner(System.in);
-		System.out.println("Enter number");
+		System.out.println("Enter number............");
 		
 		int num = sc1.nextInt();
 		int reverse = 0;
---
+++ src/string/HelloPractice.java
@@ -0,0 +1,11 @@
+package string;
+
+public class HelloPractice {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Hello hi........");
+	}
+
+}
